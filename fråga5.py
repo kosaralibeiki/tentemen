25d70961d1d2a4332c7f592c813a42a246e072c2
Kosar Alibeiki <kosaralibeiki@gmail.com>
2025-10-31 11:05:23 +0100

    5 completed

---
+++ fråga5.py
@@ -0,0 +1,26 @@
+def second_smallest_and_largest(num_list):
+    unique_list = []
+    for num in num_list:
+        if num not in unique_list:
+            unique_list.append(num)
+
+    unique_list.sort()
+
+    if len(unique_list) == 1:
+        return f"({unique_list[0]}, {unique_list[0]})"
+
+    elif len(unique_list) == 2:
+        return f"({unique_list[0]}, {unique_list[1]})"
+
+    elif len(unique_list) == 0:
+        return None
+
+    else:
+        return f"({unique_list[1]}, {unique_list[-2]})"
+
+
+
+print(second_smallest_and_largest([2,2,2,4,6,-1]))
+print(second_smallest_and_largest([-1,-1]))
+print(second_smallest_and_largest([]))
+print(second_smallest_and_largest([3]))
